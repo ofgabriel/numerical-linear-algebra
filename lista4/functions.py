c9f947978c1badebbba35256c496131f3cdc178d
Gabriel Oliveira <gabriel.oliveira@poli.ufrj.br>
2018-05-21 10:23:49 -0300

    Adjusts encoding.

--- lista4/functions.py
+++ lista4/functions.py
@@ -14,7 +14,7 @@ def one(x):
     part = cosh(x*sqrt(9.806*0.00341))
     return log10(part)-50
     #Bissection: for the interval [600,700] and tol = .0001, the root is 633.387470245.
-    #Newton: for x0 = 650, tol = 5x10⁻⁴ and niter = 10, the root is 633.387419103.
+    #Newton: for x0 = 650, tol = 5x10^-4 and niter = 10, the root is 633.387419103.
 
 def dif_one(x):
     y = one(x)
@@ -25,7 +25,7 @@ def dif_one(x):
 def two(x):
     return 4*cos(x) - exp(2*x)
     #Bissection: for the interval [-2,-1] and tol = .0001, the root is -1.55975341797.
-    #Newton: for x0 = -2, tol = 5x10⁻⁴ and niter = 10, the root is -1.55966887236.
+    #Newton: for x0 = -2, tol = 5x10^-4 and niter = 10, the root is -1.55966887236.
 
 def dif_two(x):
     y = two(x)
