bfcd9772fbbb1eac0c4ffdac42d5e37806600ef0
Lucas Vieira <lucasvg@poli.ufrj.br>
2018-05-26 00:31:40 -0300

    Refactor + evaluation of F

--- lista4/newtonSystem.py
+++ lista4/newtonSystem.py
@@ -1,10 +1,14 @@
 from functions import *
 
+tol = 5*10**-4
+niter = 10
+
 # funcs : vetor of functions
 # X     : vetor of initial values for the funcs params
 def newtonSystem(X, tol, niter, funcs):
-    for k in range(niter):
-        i = 1
+    print jacobianMatrix(funcs, X)
+    F = [func(X) for func in funcs]
+    print F
 
 def func1(X):
     return X[0] + 2*X[1] - 2
@@ -14,4 +18,5 @@ def func2(X):
 
 X = [2, 3]
 funcs = [func1, func2]
-print jacobianMatrix(funcs, X)
+
+newtonSystem(X, tol, niter, funcs)
