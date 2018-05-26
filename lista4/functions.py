c0ee424d4a81c3dbd30d3bef609d6c60a068cbb5
Lucas Vieira <lucasvg@poli.ufrj.br>
2018-05-25 23:36:05 -0300

    Calculates jacobian matrix

--- lista4/functions.py
+++ lista4/functions.py
@@ -41,3 +41,32 @@ def dif(fun,x):
     dx = 1**-10
     dy = fun(x+dx)
     return (dy - y)/dx
+
+# func   : function that receives an array of params
+# X      : array of params
+# xIndex : index of the parameter of the derivative
+def partDerivative(func, X, xIndex):
+    dx = 1**-10
+    y1 = func(X)
+    X[xIndex] += dx
+    y2 = func(X)
+    X[xIndex] -= dx # keeps X unchanged
+    return (y2 - y1)/dx
+
+# creates matrix with m rows, n columns filled with value
+def createMatrix(m,n,value):
+    matrix = []
+    for i in range(m):
+        matrix.append([])
+        for j in range(n):
+            matrix[i].append(value)
+    return matrix
+
+# X : vetor of paramentes for X
+# funcs : vetor of functions
+def jacobianMatrix(funcs, X):
+    J = createMatrix(len(funcs), len(X), None)
+    for i in range(len(funcs)):
+        for j in range(len(X)):
+            J[i][j] = partDerivative(funcs[i], X, j)
+    return J
--- lista4/newtonSystem.py
+++ lista4/newtonSystem.py
@@ -1,4 +1,17 @@
 from functions import *
 
-def newtonSystem(x,tol,niter):
+# funcs : vetor of functions
+# X     : vetor of initial values for the funcs params
+def newtonSystem(X, tol, niter, funcs):
     for k in range(niter):
+        i = 1
+
+def func1(X):
+    return X[0] + 2*X[1] - 2
+
+def func2(X):
+    return X[0]**2 + 4*(X[1]**2) -4
+
+X = [2, 3]
+funcs = [func1, func2]
+print jacobianMatrix(funcs, X)
