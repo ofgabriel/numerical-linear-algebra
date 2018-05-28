4c093de3a7f813165dd3d0ad881d6660c81d81ca
Gabriel Oliveira <gabriel.oliveira@poli.ufrj.br>
2018-05-28 18:32:37 -0300

    Excludes specific dif functions. They are not necessary.

--- lista4/functions.py
+++ lista4/functions.py
@@ -9,12 +9,6 @@ from mathMethods import multiMM, multiMS
 def test(x):
     return (x**2 - 4*cos(x))
 
-def dif_test(x):
-    y = test(x)
-    dx = 1**-10
-    dy = test(x+dx)
-    return (dy - y)/dx
-
 def one(x):
     part = cosh(x*sqrt(9.806*0.00341))
     return log10(part)-50
@@ -22,24 +16,12 @@ def one(x):
     #Secante: for x0 = 650, tol = 5x10^-4 and niter = 10, the root is 633.387419103.
     #Newton: for x0 = 650, tol = 5x10^-4 and niter = 10, the root is 633.387419103.
 
-def dif_one(x):
-    y = one(x)
-    dx = 1**-10
-    dy = one(x+dx)
-    return (dy - y)/dx
-
 def two(x):
     return 4*cos(x) - exp(2*x)
     #Bissection: for the interval [-2,-1] and tol = .0001, the root is -1.55975341797.
     #Newton: for x0 = -2, tol = 5x10^-4 and niter = 10, the root is -1.55966887236.
     #Secante: for x0 = -2, tol = 5x10^-4 and niter = 10, the root is -1.55975081185.
     #In fact, there are multiples roots and the result depends on the x0 defined.
-
-def dif_two(x):
-    y = two(x)
-    dx = 1**-10
-    dy = two(x+dx)
-    return (dy - y)/dx
 
 def dif(fun,x):
     y = fun(x)
