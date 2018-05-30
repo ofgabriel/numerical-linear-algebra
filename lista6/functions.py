a406e8566873290654500ea8dd6846fd662073d4
Lucas Vieira <lucasvg@poli.ufrj.br>
2018-05-30 15:55:09 -0300

    Adds method rungeKutta4

--- lista6/functions.py
+++ lista6/functions.py
@@ -37,6 +37,25 @@ def rungeKutta2(funcDifX, deltaT, tI, funcTIResult, tF):
         funcResults[i] = funcResults[i-1] + (deltaT/2) * (K1 + K2)
     return funcResults
 
+def rungeKutta4(funcDifX, deltaT, tI, funcTIResult, tF):
+    NITER = 1 + (tF-tI)/deltaT
+    if NITER != int(NITER):
+        print 'Exception: supposed to be integer'
+        sys.exit()
+    NITER = int(NITER)
+
+    funcResults = [None for i in range(NITER)]
+    funcResults[0] = funcTIResult
+
+    for i in range(1, NITER):
+        t = tI + (i-1)*deltaT
+        K1 = funcDifX(t, funcResults[i-1])
+        K2 = funcDifX(t + deltaT/2, funcResults[i-1] + (deltaT/2)*K1)
+        K3 = funcDifX(t + deltaT/2, funcResults[i-1] + (deltaT/2)*K2)
+        K4 = funcDifX(t + deltaT, funcResults[i-1] + deltaT*K3)
+        funcResults[i] = funcResults[i-1] + (deltaT/6) * (K1 + 2*K2 + 2*K3 + K4)
+    return funcResults
+
 def enum(**enums):
     return type('Enum', (), enums)
 
@@ -47,6 +66,8 @@ def integral(method, funcDifX, deltaT, tI, funcTIResult, tF):
         derivateResults = euler(funcDifX, deltaT, tI, funcTIResult, tF)
     elif(method == INTEGRAL_METHOD.RUNGE_KUTTA_2):
         derivateResults = rungeKutta2(funcDifX, deltaT, tI, funcTIResult, tF)
+    elif(method == INTEGRAL_METHOD.RUNGE_KUTTA_4):
+        derivateResults = rungeKutta4(funcDifX, deltaT, tI, funcTIResult, tF)
 
     print derivateResults
     return derivateResults[-1] - derivateResults[0]
