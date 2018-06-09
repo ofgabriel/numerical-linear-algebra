c47eb86f1ba678d7728b5958add6c72c2adb9bb5
Lucas Vieira <lucasvg@poli.ufrj.br>
2018-06-09 14:20:17 -0300

    Adds buggy taylor solution

---
+++ lista6/taylorExampleFromSlides.py
@@ -0,0 +1,12 @@
+﻿from functions import *
+
+def funcDif2X(t, X, difX):
+    return (-1)*9.8 - difX*abs(difX)
+
+deltaT = 0.1
+tI = 0.0
+xI = 0
+difXI = 0
+tF = 1.0
+
+print 'result: ' + str(integral2ndOrder(INTEGRAL_2_METHOD.TAYLOR, funcDif2X, tI, xI, difXI, deltaT, tF))
