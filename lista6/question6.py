e1a9037dd5d018b33ea75d18db9a3a3e69fd2137
Lucas Vieira <lucasvg@poli.ufrj.br>
2018-06-12 18:56:57 -0300

    Adds question6

---
+++ lista6/question6.py
@@ -0,0 +1,20 @@
+﻿from functions import *
+import matplotlib.pyplot as plt
+
+def funcDifY(t, funcY):
+    return -10*t*funcY**2
+
+deltaT = 0.1
+tI = 0.0
+yI = 2
+tF = 1.0
+
+result = rungeKutta2(funcDifY, deltaT, tI, yI, tF)
+
+pts = [(tI + deltaT*i) for i in range(0, int(1 + (tF-tI)/deltaT))]
+
+print pts 
+plt.figure(1)
+plt.plot(pts,result,'ro')
+plt.ylabel('Runge Kutta')
+plt.show() 
