589de6cad0205eb70b16d9d18ce7c33946e838e7
Lucas Vieira <lucasvg@poli.ufrj.br>
2018-06-12 15:48:48 -0300

    Moves function to generic file

--- lista4/bissecao.py
+++ lista4/bissecao.py
@@ -1,15 +1,3 @@
 from functions import *
 
-def bissec(a,b,tol,fun):
-    #Returns the root of a function given an interval [a,b] and a tol.
-    while abs(b-a) > tol:
-        x = float(a+b)/2
-        f = fun(x)
-        #Replace the number of the function defined in the archive.
-        if f > 0.0:
-            b = x
-        else:
-            a = x
-    return x
-
 print bissec(-2,-1,0.0001,two)
--- lista4/functions.py
+++ lista4/functions.py
@@ -161,3 +161,15 @@ def leastSquareNL(B, tol, niter, funcs):
             return B
 
     print("Convergencia nao atingida.")
+
+def bissec(a,b,tol,fun):
+    #Returns the root of a function given an interval [a,b] and a tol.
+    while abs(b-a) > tol:
+        x = float(a+b)/2
+        f = fun(x)
+        #Replace the number of the function defined in the archive.
+        if f > 0.0:
+            b = x
+        else:
+            a = x
+    return x
